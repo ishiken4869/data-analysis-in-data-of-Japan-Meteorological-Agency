58195d2f2d5fdd81b4a8e8ef7caa9b4d4a2f6802
ishiken4869 <118356179+ishiken4869@users.noreply.github.com>
2023-11-09 13:26:54 +0900

    平均気温のHTMLを追加

--- index.py
+++ index.py
@@ -7,6 +7,7 @@ image = "static/images/japanmap.png"
 @app.route('/', methods=['GET', 'POST'])
 def index():
     title = "気象庁のデータ可視化"
+
     try:
         if request.method == 'POST':
             region = request.form['region']
@@ -21,6 +22,26 @@ def index():
         return render_template("index.html", title=title, region=region, year=year, month=month)
     except Exception as e:
         return render_template("index.html", title=title, region="地方の取得に失敗", year="年の取得に失敗", month="月の取得に失敗")
+
+@app.route('/temperture', methods=['GET', 'POST'])
+def temperture():
+    title = "地方別の過去の平均気温との有意さのマッピング"
+
+    try:
+        if request.method == 'POST':
+            region = request.form['region']
+            year = request.form['year']
+            month = request.form['month']
+
+        else:
+            region = request.args.get('region')
+            year = request.args.get('year')
+            month = request.args.get('month')
+
+        return render_template("temperture.html", title=title, region=region, year=year, month=month)
+    except Exception as e:
+        return render_template("temperture.html", title=title, region="地方の取得に失敗", year="年の取得に失敗", month="月の取得に失敗")
+
 
 @app.route('/map', methods=["POST", "GET"])
 def map():
